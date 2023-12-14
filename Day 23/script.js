c433ee15263a9e47567fcc9c45888b25046dd51f
Shanur <shanurislam@gmail.com>
2023-12-14 04:32:05 +0000

    formatting fixes

--- Day 23/script.js
+++ Day 23/script.js
@@ -1,10 +1,11 @@
 // Things that can be improved
 // 1 - display the key and value in the local storage data after submission - DONE
-// 2 - check if a key already exists with a corresponding value - DONE
+// 2 - check if a key already exists and update corresponding value - DONE
 // 3 - display notice when there is no data stored in local storage - DONE
-// 4 - add a counter of how much data is stored in local storage and how my storage space is remaining
+// 4 - add a counter of how much data is stored in local storage
 // 5 - improve the display of local storage data - DONE
 // 6 - export local storage data
+// 7 - search for partial string matches
 
 document.addEventListener('DOMContentLoaded', function() {
 
@@ -22,10 +23,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const saveToLocalStorage = () => {
         console.log('saveToLocalStorage');
         localStorageBody.innerHTML = '';
-
         let key = keyInput.value;
         const value = valueInput.value;
-
         if (key && value){
             key = key.charAt(0).toUpperCase() + key.slice(1).toLowerCase();
             if (localStorage.getItem(key) !== null){
@@ -41,58 +40,58 @@ document.addEventListener('DOMContentLoaded', function() {
     };
 
     // Function to read Local Storage and display in HTML
-    const readLocalStorage = () => {
+    const readLocalStorage = () =>{
         console.log('readLocalStorage');
         localStorageBody.innerHTML = '';
-
-        for (let i = 0; i < localStorage.length; i++) {
-            let key = localStorage.key(i);
-            let value = localStorage.getItem(key);
-            displayRow(key, value);
-        }
-    };
+        if (localStorage.length = 0){
+            for (let i = 0; i < localStorage.length; i++) {
+                let key = localStorage.key(i);
+                let value = localStorage.getItem(key);
+                displayRow(key, value);
+            }
+        } else {
+            const row = document.createElement('tr');
+            row.innerHTML = `<td>No data in local storage.</td><td></td>`;
+            localStorageBody.appendChild(row);
+    }};
 
     // Function to search local storage
     function searchLocalStorage(e) {
         console.log('searchLocalStorage');
-        const key = e;
-        const value = localStorage.getItem(key);
-
+        const value = localStorage.getItem(e);
         if (value !== null) {
-            displayRow(key, value);
+            displayRow(e, value);
         } else {
-            displayNotFoundRow(key);
-        }
-    };
+            displayNotFoundRow(e);
+    }};
 
     function displayRow(key, value){
         console.log('displayRow');
         const row = document.createElement('tr');
         row.innerHTML = `<td>${key}</td><td>${value}</td>`;
         localStorageBody.appendChild(row);
-    }
+    };
 
     function displayNotFoundRow(key) {
         console.log('displayNotFoundRow');
         const row = document.createElement('tr');
-        row.innerText = `Key not found: ${key}`;
+        row.innerHTML = `<td>Key not found: ${key}</td><td></td>`;
         localStorageBody.appendChild(row);
-    }
+    };
 
     // Event listeners
     // Save data to local storage
     addBtn.addEventListener('click', () =>{
         console.log('addBtn');
         saveToLocalStorage();
-    })
+    });
 
     // Button to search or view local storage
     searchViewBtn.addEventListener('click', () =>{
         console.log('searchViewBtn');
         localStorageBody.innerHTML = '';
-    
         let key = searchInput.value.trim();
-
+        key = key.charAt(0).toUpperCase() + key.slice(1).toLowerCase();
         if (key.length > 0){
             searchLocalStorage(key);
         } else {
