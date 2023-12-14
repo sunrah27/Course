3a0ca4b9cc1efc057fbcd34ce5b845b636ef6dab
Shanur <shanurislam@gmail.com>
2023-12-14 04:14:33 +0000

    Local storage v2

--- Day 23/script.js
+++ Day 23/script.js
@@ -1,88 +1,118 @@
 // Things that can be improved
-// 1 - display the key and value in the local storage data after submission
-// 2 - check if a key already exists with a corresponding value
-// 3 - display notice when there is no data stored in local storage
+// 1 - display the key and value in the local storage data after submission - DONE
+// 2 - check if a key already exists with a corresponding value - DONE
+// 3 - display notice when there is no data stored in local storage - DONE
 // 4 - add a counter of how much data is stored in local storage and how my storage space is remaining
-// 5 - improve the display of local storage data
-// 6 - import local storage data
+// 5 - improve the display of local storage data - DONE
+// 6 - export local storage data
 
-// Access all of our relevant elements from the DOM
-const inputKey = document.getElementById('key');
-const inputValue = document.getElementById('value');
-const submitBtn = document.getElementById('submitBtn');
-const clearBtn = document.getElementById('clearBtn');
-const readLSBtn = document.getElementById('readLSBtn');
-const storageReadOut = document.getElementById('storageReadOut');
-const resetLSBtn = document.getElementById('resetLSBtn');
-const searchInput = document.getElementById('searchLS');
+document.addEventListener('DOMContentLoaded', function() {
 
-// Function to save key, value pair to local storage;
-const saveToLocalStorage = (e) => {
-    e.preventDefault();
+    // Access all of our relevant elements from the DOM
+    const keyInput = document.getElementById('keyInput');
+    const valueInput = document.getElementById('valueInput');
+    const addBtn = document.getElementById('addBtn');
+    const searchInput = document.getElementById('searchInput');
+    const searchViewBtn = document.getElementById('searchViewBtn');
+    const resetBtn = document.getElementById('resetBtn');
+    const clearBtn = document.getElementById('clearBtn');
+    const localStorageBody = document.getElementById('localStorageBody');
 
-    const key = inputKey.value;
-    const value = inputValue.value;
+    // Function to save key, value pair to local storage
+    const saveToLocalStorage = () => {
+        console.log('saveToLocalStorage');
+        localStorageBody.innerHTML = '';
 
-    if (key && value) {
-        localStorage.setItem(key,value);
-        inputKey.value = '';
-        inputValue.value = '';
+        let key = keyInput.value;
+        const value = valueInput.value;
+
+        if (key && value){
+            key = key.charAt(0).toUpperCase() + key.slice(1).toLowerCase();
+            if (localStorage.getItem(key) !== null){
+                localStorage.setItem(key, value);
+            } else {
+                localStorage.setItem(key, value);
+            }
+            keyInput.value = '';
+            valueInput.value = '';
+            searchInput.value = '';
+        }
+        searchLocalStorage(key);
+    };
+
+    // Function to read Local Storage and display in HTML
+    const readLocalStorage = () => {
+        console.log('readLocalStorage');
+        localStorageBody.innerHTML = '';
+
+        for (let i = 0; i < localStorage.length; i++) {
+            let key = localStorage.key(i);
+            let value = localStorage.getItem(key);
+            displayRow(key, value);
+        }
+    };
+
+    // Function to search local storage
+    function searchLocalStorage(e) {
+        console.log('searchLocalStorage');
+        const key = e;
+        const value = localStorage.getItem(key);
+
+        if (value !== null) {
+            displayRow(key, value);
+        } else {
+            displayNotFoundRow(key);
+        }
+    };
+
+    function displayRow(key, value){
+        console.log('displayRow');
+        const row = document.createElement('tr');
+        row.innerHTML = `<td>${key}</td><td>${value}</td>`;
+        localStorageBody.appendChild(row);
     }
-};
 
-// read Local Storage and display in HTML
-function readLocalStorage() {
-    storageReadOut.innerHTML = '';
-    for (let i = 0; i < localStorage.length; i++) {
-        let myKey = localStorage.key(i);
-        let myValue = localStorage.getItem(myKey);
-        let newRead = document.createElement('p');
-        newRead.innerHTML = `Key: <span class="spanKey">${myKey}</span> Value: <span class="spanValue">${myValue}</span>`;
-        storageReadOut.append(newRead);
+    function displayNotFoundRow(key) {
+        console.log('displayNotFoundRow');
+        const row = document.createElement('tr');
+        row.innerText = `Key not found: ${key}`;
+        localStorageBody.appendChild(row);
     }
-};
 
-// Function to search local storage
-function searchLocalStorage() {
-    const searchKey = searchInput.value.trim();
-    const searchValue = localStorage.getItem(searchKey);
-    const newRead = document.createElement('p');
+    // Event listeners
+    // Save data to local storage
+    addBtn.addEventListener('click', () =>{
+        console.log('addBtn');
+        saveToLocalStorage();
+    })
 
-    if (searchValue !== null) {
-        newRead.innerHTML = `Key: <span class="spanKey">${searchKey}</span> Value: <span class="spanValue">${searchValue}</span>`;
-        console.log(newRead);
-        storageReadOut.append(newRead);
-    } else {
-        newRead.innerText = `Key not found: ${searchKey}`;
-        storageReadOut.append(newRead);
-    }
-}
+    // Button to search or view local storage
+    searchViewBtn.addEventListener('click', () =>{
+        console.log('searchViewBtn');
+        localStorageBody.innerHTML = '';
+    
+        let key = searchInput.value.trim();
 
-// Event listners
-readLSBtn.addEventListener('click', (e) =>{
-    e.preventDefault();
-    storageReadOut.innerHTML = '';
-    let searchKey = searchInput.value.trim();
-    let searchValue = localStorage.getItem(searchKey);
-    if (searchValue !== null){
-        searchLocalStorage();
-    } else {
-        readLocalStorage();
-    }
-});
+        if (key.length > 0){
+            searchLocalStorage(key);
+        } else {
+            readLocalStorage();
+        }
+    });
 
-// reset the local storage display
-resetLSBtn.addEventListener('click', (e) =>{
-    e.preventDefault();
-    storageReadOut.innerHTML = 'Local storage data';
-    searchInput.value ='';
-});
+    // Button to reset the local storage display
+    resetBtn.addEventListener('click', () =>{
+        console.log('resetBtn');
+        localStorageBody.innerHTML = '';
+        keyInput.value = '';
+        valueInput.value = '';
+        searchInput.value = '';
+    });
 
-// Save data to local storage
-submitBtn.addEventListener('click', saveToLocalStorage);
-
-// clear Local Storage
-clearBtn.addEventListener('click', () =>{
-    localStorage.clear();
-    location.reload();
+    // Button to clear Local Storage
+    clearBtn.addEventListener('click', () =>{
+        console.log('clearBtn');
+        localStorage.clear();
+        location.reload();
+    });
 });
